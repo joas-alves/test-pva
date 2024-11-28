8f1bfff2e6213c6eb5279b6b2bf01bce2fb098de
casimir6 <kaminskicasimir6@gmail.com>
2024-11-28 19:34:43 +0100

    fix: padding issue of mobile

--- src/components/widgets/SignUpInput.tsx
+++ src/components/widgets/SignUpInput.tsx
@@ -5,7 +5,7 @@ export const SignUpInput = () => {
 
   return (
     <form className="flex items-center max-w-[410px] w-full rounded-2xl border border-gray-300 p-0.5">
-      <input className="flex-1 text-sm px-4 sm:px-6" placeholder="Please enter your E-mail" />
+      <input className="flex-1 text-sm pl-4 pr-0 sm:px-6" placeholder="Please enter your E-mail" />
       <button className="btn primary-btn min-w-[113px]">{t('signup')}</button>
     </form>
   )
