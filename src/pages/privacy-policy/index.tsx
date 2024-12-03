9556eb98c4ae24d4be971cad952d08491ed8fbab
Abdullah <mabdullahkhan009@gmail.com>
2024-12-03 19:08:23 +0500

    feat(page): privacy policy page

---
+++ src/pages/privacy-policy/index.tsx
@@ -0,0 +1,33 @@
+import { ContactUsForm } from "@/components/widgets";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { PrivacyIntroduction } from "@/components/privacy-policy/Introduction";
+import { PrivacyWhoWeAre } from "@/components/privacy-policy/WhoWeAre";
+import { PrivacyPersonalData } from "@/components/privacy-policy/PersonalData";
+import { PrivacyProcessing } from "@/components/privacy-policy/Processing";
+import { PrivacyYourRights } from "@/components/privacy-policy/YourRights";
+import { PrivacyRetention } from "@/components/privacy-policy/Retention";
+import { PrivacyOtherWebsites } from "@/components/privacy-policy/OtherWebsites";
+import { PrivacyCookies } from "@/components/privacy-policy/Cookies";
+import { PrivacyChanges } from "@/components/privacy-policy/Changes";
+import { PrivacyCompliance } from "@/components/privacy-policy/Compliance";
+
+export default function PrivacyPolicy() {
+  return (
+    <section className="container mx-auto pt-10 pb-40">
+      <h1 className="sub-heading mb-6">
+        <span className="text-primary">Privacy</span> Policy
+      </h1>
+      <PrivacyIntroduction/>
+      <PrivacyWhoWeAre/>
+      <PrivacyPersonalData/>
+      <PrivacyProcessing/>
+      <PrivacyYourRights/>
+      <PrivacyRetention/>
+      <PrivacyOtherWebsites/>
+      <PrivacyCookies/>
+      <PrivacyChanges/>
+      <PrivacyCompliance/>
+    </section>
+  );
+}
