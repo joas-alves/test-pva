b3473f0b1a390c9fb5fd3da6b9ae75681c9fe01c
Abdullah <mabdullahkhan009@gmail.com>
2024-12-05 10:34:29 -0800

    pdfs core(download and view pdfs feature added)

--- src/components/investors/GeneralMeetingSection.tsx
+++ src/components/investors/GeneralMeetingSection.tsx
@@ -1,8 +1,23 @@
+import React from 'react';
 import { MeetingLink } from "@/components/widgets";
 import { useTranslations } from "next-intl";
 
 export const GeneralMeetingSection = () => {
   const t = useTranslations('Investors');
+
+
+  const downloadPdf = (download: boolean, link: string) => {
+    if (download) {
+      const linkElement = document.createElement('a');
+      linkElement.href = link;
+      linkElement.download = 'dummy.pdf';
+      document.body.appendChild(linkElement);
+      linkElement.click();
+      document.body.removeChild(linkElement);
+    } else {
+      window.open(link, '_blank');
+    }
+  };
 
   return (
     <section className="container mx-auto py-10">
@@ -12,35 +27,38 @@ export const GeneralMeetingSection = () => {
       <div className="flex justify-between flex-col md:flex-row border-b border-b-gray-200 py-10">
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2024</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
-          <MeetingLink name="2024-AGM-Notice-Final" />
-          <MeetingLink name="2023-PVG-Annual-Report-FINAL" />
-          <MeetingLink name="2024-AGM-Votes-Final" />
+          <MeetingLink name="2024-AGM-Notice-Final" downloadPdf={()=>{}} />
+          <MeetingLink name="2023-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink name="2024-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
+
       <div className="flex justify-between flex-col md:flex-row border-b border-b-gray-200 py-10">
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2023</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
-          <MeetingLink name="2024-AGM-Notice-Final" />
-          <MeetingLink name="2023-PVG-Annual-Report-FINAL" />
-          <MeetingLink name="2024-AGM-Votes-Final" />
+          <MeetingLink name="2024-AGM-Notice-Final" downloadPdf={downloadPdf} />
+          <MeetingLink name="2023-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink name="2024-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
+
       <div className="flex justify-between flex-col md:flex-row border-b border-b-gray-200 py-10">
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2022</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
-          <MeetingLink name="2024-AGM-Notice-Final" />
-          <MeetingLink name="2023-PVG-Annual-Report-FINAL" />
-          <MeetingLink name="2024-AGM-Votes-Final" />
+          <MeetingLink name="2024-AGM-Notice-Final" downloadPdf={downloadPdf} />
+          <MeetingLink name="2023-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink name="2024-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
+
       <div className="flex justify-between flex-col md:flex-row py-10">
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2021</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
-          <MeetingLink name="2024-AGM-Notice-Final" />
-          <MeetingLink name="2023-PVG-Annual-Report-FINAL" />
-          <MeetingLink name="2024-AGM-Votes-Final" />
+          <MeetingLink name="2024-AGM-Notice-Final" downloadPdf={downloadPdf} />
+          <MeetingLink name="2023-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink name="2024-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
