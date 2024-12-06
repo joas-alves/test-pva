bfab6becefa0e3c9efe3078e08839c7f8712778e
Abdul Wasay <abdul.wasay@wanologicalsolutions.com>
2024-12-06 20:08:43 +0500

    feat(app): declare global google and googleTranslateElementInit for translation integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,7 @@ setupAxios();
 
 declare global {
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     google: any;
     googleTranslateElementInit: () => void;
   }
