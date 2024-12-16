b9d3a0d9acb33e15d0f2808b45ab9a2512ebb157
Abdullah <mabdullahkhan009@gmail.com>
2024-12-17 00:45:35 +0500

    fix(ui): implemented changes

--- src/components/services/EnhancedSection.tsx
+++ src/components/services/EnhancedSection.tsx
@@ -1,10 +1,10 @@
+import { IServicePage } from "@/cms-models/service";
+import { imageUrl } from "@/utils";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
+import React from "react";
 import { PiChartLineUpLight } from "react-icons/pi";
 import { RiGlobalLine } from "react-icons/ri";
-import { useTranslations } from "next-intl";
-import { IServicePage } from "@/cms-models/service";
-import React from "react";
-import { imageUrl } from "@/utils";
 
 type Props = {
   data: IServicePage;
@@ -15,37 +15,49 @@ export const EnhancedSection: React.FC<Props> = ({ data }) => {
 
   return (
     <section className="container mx-auto py-10 flex flex-col gap-20">
-      <div className="flex flex-col gap-4 md:gap-0 md:flex-row items-center md:items-start mx-auto">
-        <div className="bg-white rounded-3xl shadow-paper p-6 md:p-9 w-full max-w-[302px] flex-shrink-0 relative z-10 md:mt-20 md:-mr-20">
-          <div className="flex justify-start mb-4">
-            <span className="w-10 md:w-[60px] h-10 md:h-[60px] bg-primary rounded-lg md:rounded-2xl flex items-center justify-center">
-              <PiChartLineUpLight className="text-white text-xl md:text-2xl" />
-            </span>
-          </div>
-          <div className="text-center">
-            <div className="text-3xl font-bold text-gray-900 mb-2">
-              {data.section2_practice_number}
+      <div className="flex flex-col lg:flex-row gap-20">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col gap-4 md:gap-0 md:flex-row items-center md:items-start mx-auto">
+            <div className="bg-white rounded-3xl shadow-paper p-6 md:p-9 w-full max-w-[302px] flex-shrink-0 relative z-10 md:mt-20 md:-mr-20">
+              <div className="flex justify-start mb-4">
+                <span className="w-10 md:w-[60px] h-10 md:h-[60px] bg-primary rounded-lg md:rounded-2xl flex items-center justify-center">
+                  <PiChartLineUpLight className="text-white text-xl md:text-2xl" />
+                </span>
+              </div>
+              <div className="text-center">
+                <div className="text-3xl font-bold text-gray-900 mb-2">
+                  {data.section2_practice_number}
+                </div>
+                <div className="text-sm md:text-base text-secondary">
+                  {t('number_of_practices_we_operate_in')}
+                </div>
+              </div>
+              <div className="flex justify-end mt-4">
+                <span className="w-10 md:w-[60px] h-10 md:h-[60px] bg-info rounded-lg md:rounded-2xl flex items-center justify-center">
+                  <RiGlobalLine className="text-white text-xl md:text-2xl" />
+                </span>
+              </div>
             </div>
-            <div className="text-sm md:text-base text-secondary">
-              {t('number_of_practices_we_operate_in')}
+            <div className="relative z-0">
+              <Image
+                className="rounded-3xl w-full shadow-md"
+                src={imageUrl(data.section2_image)}
+                width={412}
+                height={365}
+                alt="dog"
+              />
             </div>
-          </div>
-          <div className="flex justify-end mt-4">
-            <span className="w-10 md:w-[60px] h-10 md:h-[60px] bg-info rounded-lg md:rounded-2xl flex items-center justify-center">
-              <RiGlobalLine className="text-white text-xl md:text-2xl" />
-            </span>
-          </div>
+          </div></div>
+        <div className="w-full lg:w-1/2">
+        <div className="flex flex-col gap-4">
+          <h2 dangerouslySetInnerHTML={{__html:data.section2_title}}/>
+          <p>{data.section2_description}</p>
+
         </div>
-        <div className="relative z-0">
-          <Image
-            className="rounded-3xl w-full shadow-md"
-            src={imageUrl(data.section2_image)}
-            width={412}
-            height={365}
-            alt="dog"
-          />
         </div>
+
       </div>
+
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 md:gap-6">
         <div className="shadow-paper rounded-2xl p-6 md:p-7">
--- src/components/widgets/SmallReviewCarouselCard.tsx
+++ src/components/widgets/SmallReviewCarouselCard.tsx
@@ -1,33 +1,51 @@
 import { GoStarFill } from "react-icons/go";
 
-export const SmallReviewCarouselCard = () => {
+export type ReviewUser = {
+  name: string;
+  username: string;
+  image: string;
+};
+
+export type Review = {
+  rate: number;
+  message: string;
+  user: ReviewUser;
+};
+
+type DataProp = Review
+
+
+
+export const SmallReviewCarouselCard: React.FC<DataProp> = ({ rate, message, user: { image, name, username } }) => {
   return (
-    <div className="shadow-paper rounded-3xl p-6 md:py-8 md:px-10">
+    <div className="shadow-paper rounded-3xl p-6 md:py-8 md:px-10 h-full lg:min-h-[500px]">
+      <div className="flex justify-between items-center mb-2">
+        <div className="flex items-center gap-4">
+          <div className="w-16 h-16 rounded-full bg-gray-100">
+            {image && <img src={image} className="w-16" />}
+          </div>
+          <div>
+            <p className="text-sm md:text-base font-bold">{username}</p>
+            <p className="text-xs md:text-sm text-secondary">{name}</p>
+          </div>
+        </div>
+      </div>
       <div className="flex gap-1 mb-6">
         <div>
-          <div className="text-dark-green">Reviews</div>
           <div className="text-warning flex items-center text-sm gap-1">
             <GoStarFill />
             <GoStarFill />
             <GoStarFill />
             <GoStarFill />
             <GoStarFill />
-            <span className="text-dark-green text-sm mx-2">4.8(70)</span>
+            <span className="text-dark-green text-sm mx-2">{rate}</span>
           </div>
         </div>
       </div>
-      <div className="text-sm md:text-base font-light mb-10 md:mb-20">
-        PVA understands vets! We get helpful and clear advice from coaches, and they display great product and practice knowledge. PVA coaches clearly understand clinical staff and client needs. The retention training really helped our staff speak to our clients about what the savings they make while being on a plan and this has reduced cancellations.
+      <div className="text-sm md:text-base font-light">
+        {message}
       </div>
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <div className="w-12 h-12 md:w-16 md:h-16 rounded-full bg-gray-100"></div>
-          <div>
-            <p className="text-sm md:text-base font-bold">Katie W.</p>
-            <p className="text-xs md:text-sm text-secondary">Anderson Vets.</p>
-          </div>
-        </div>
-      </div>
+
     </div>
   )
 }
