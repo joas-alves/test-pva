8527eefe1fb8a92aa6e73e4497f253ebb35fce4b
jehanzebsherali <jehanzebsherali@gmail.com>
2024-12-16 17:57:19 +0500

    1- add check for "generate earn" image

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -26,15 +26,18 @@ export const HeroSection: React.FC<Props> = ({ data }) => {
               <div className="flex justify-center lg:justify-start">
                 <SignUpInput />
               </div>
-              <div className="mx-auto lg:mx-0">
-                <Image
-                  className=""
-                  src={imageUrl(data.section1_rating_image)}
-                  width={410}
-                  height={187}
-                  alt="generate extra euro 350,000"
-                />
+              {data.section1_rating_image && (
+                <div className="mx-auto lg:mx-0">
+                  <Image
+                    className=""
+                    src={imageUrl(data.section1_rating_image)}
+                    width={410}
+                    height={187}
+                    alt="generate extra euro 350,000"
+                  />
               </div>
+              ) }
+              
             </div>
             <Image
               className="hidden xl:block w-full max-w-96 object-contain -mt-6"
