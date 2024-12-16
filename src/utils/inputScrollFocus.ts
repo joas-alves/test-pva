5bb69ab91fb1ba0fc555d4c9574119d447778a5b
jehanzebsherali <jehanzebsherali@gmail.com>
2024-12-16 15:37:36 +0500

    resole build issue for input scroll focus

--- src/utils/inputScrollFocus.ts
+++ src/utils/inputScrollFocus.ts
@@ -1,4 +1,5 @@
-export const handleScrollToInput = (inputRef: any) => {
+import { MutableRefObject } from "react";
+export const handleScrollToInput = (inputRef: MutableRefObject<HTMLInputElement | null>) => {
   if (inputRef.current) {
     inputRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
     inputRef.current.focus();
