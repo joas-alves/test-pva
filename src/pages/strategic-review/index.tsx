3acb241a7a3586b6a48dc4d37152cd5baf1ca92f
Abdullah <mabdullahkhan009@gmail.com>
2025-01-03 23:47:04 +0500

    refactor(strategic review): adding a pop up

--- src/pages/strategic-review/index.tsx
+++ src/pages/strategic-review/index.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import { NextRouter, useRouter } from "next/router";
 import { StrategicReviewSection } from "@/components/investors/StrategicReview";
+import { BasicModal } from "@/components/modals/basic";
+export default function StrategicReview() {
+  const router: NextRouter = useRouter();
 
-export default function Investors() {
+  const [isOpen, setIsOpen] = useState(false);
+  const onAction = (value: string) => {
+    if (value === "disagree") {
+      setIsOpen(false);
+      router.push("/");
+    } else {
+      localStorage.setItem("investorAgree", "true");
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem("investorAgree")) {
+      setIsOpen(true);
+    }
+  }, []);
   return (
     <div className="flex flex-col gap-0 md:gap-20">
       <StrategicReviewSection />
+      <BasicModal isOpen={isOpen} onAction={onAction} />
     </div>
   );
 }
