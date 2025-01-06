80affbc2bd438ed34ee4da627130833305619ff6
Abdullah <mabdullahkhan009@gmail.com>
2025-01-07 00:28:54 +0500

    fix(page): call homepage and globalpage content based on global and other lang

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,11 +20,15 @@ export default function Home() {
 
   const [data, setData] = useState<IHomePage>({} as IHomePage);
 
+  const url = `/en-UK/${
+    router.locale === "global" ? "globalpage-content" : "homepage-content"
+  }/1`;
+
   useEffect(() => {
     if (!router.isReady) return;
     (async () => {
       try {
-        const response = await axios(`/en-UK/homepage-content/1`);
+        const response = await axios(url);
         if (response.status === 200) {
           setData(response.data);
         }
