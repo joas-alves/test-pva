7202724efad418cae753190cdfdfd9b468ae528d
Abdullah <mabdullahkhan009@gmail.com>
2025-01-23 22:25:57 +0000

    fix(code): fixed form init

--- src/components/widgets/get-in-touch-form/index.tsx
+++ src/components/widgets/get-in-touch-form/index.tsx
@@ -33,7 +33,7 @@ export const GetInTouchForm = () => {
     postCode: "",
     planReference: "",
     customerType: PvaCustomerType.New,
-    primaryReason: "",
+    primaryReason: NewCustomerReasons.HealthPlan,
     secondaryReason: "",
     reasonComments: "",
   };
