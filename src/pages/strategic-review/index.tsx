6f2cfa2614e3c2207f0b5da5bf1e4e0d91f0c853
Abdullah <mabdullahkhan009@gmail.com>
2025-02-13 21:20:39 +0500

    fix(code): fix strategic review page code

--- src/pages/strategic-review/index.tsx
+++ src/pages/strategic-review/index.tsx
@@ -1,6 +1,8 @@
 import { StrategicReviewSection } from "@/components/investors/StrategicReview";
+import { NextRouter, useRouter } from "next/router";
+import { useEffect } from "react";
 export default function StrategicReview() {
-  // const router: NextRouter = useRouter();
+  const router: NextRouter = useRouter();
 
   // const [isOpen, setIsOpen] = useState(false);
   // const onAction = (value: string) => {
@@ -19,12 +21,12 @@ export default function StrategicReview() {
   //   }
   // }, []);
 
-  // useEffect(() => {
-  //   if (router.locale !== "global") {
-  //     // Redirect to the homepage if locale is not allowed
-  //     router.replace("/");
-  //   }
-  // }, [router.locale]);
+  useEffect(() => {
+    if (router.locale !== "global") {
+      // Redirect to the homepage if locale is not allowed
+      router.replace("/");
+    }
+  }, [router.locale]);
   return (
     <div className="flex flex-col gap-0 md:gap-20">
       <StrategicReviewSection />
