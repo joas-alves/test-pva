b83aaddbe0aa0022cfffd852759e4f6c01c8aa48
Abdullah <mabdullahkhan009@gmail.com>
2025-02-13 19:56:26 +0500

    chore(code): remove pop up from strategic review page

--- src/pages/strategic-review/index.tsx
+++ src/pages/strategic-review/index.tsx
@@ -1,37 +1,34 @@
-import { useEffect, useState } from "react";
-import { NextRouter, useRouter } from "next/router";
 import { StrategicReviewSection } from "@/components/investors/StrategicReview";
-import { BasicModal } from "@/components/modals/basic";
 export default function StrategicReview() {
-  const router: NextRouter = useRouter();
+  // const router: NextRouter = useRouter();
 
-  const [isOpen, setIsOpen] = useState(false);
-  const onAction = (value: string) => {
-    if (value === "disagree") {
-      setIsOpen(false);
-      router.push("/");
-    } else {
-      localStorage.setItem("strategicReviewAgree", "true");
-      setIsOpen(false);
-    }
-  };
+  // const [isOpen, setIsOpen] = useState(false);
+  // const onAction = (value: string) => {
+  //   if (value === "disagree") {
+  //     setIsOpen(false);
+  //     router.push("/");
+  //   } else {
+  //     localStorage.setItem("strategicReviewAgree", "true");
+  //     setIsOpen(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    if (!localStorage.getItem("strategicReviewAgree")) {
-      setIsOpen(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (!localStorage.getItem("strategicReviewAgree")) {
+  //     setIsOpen(true);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    if (router.locale !== "global") {
-      // Redirect to the homepage if locale is not allowed
-      router.replace("/");
-    }
-  }, [router.locale]);
+  // useEffect(() => {
+  //   if (router.locale !== "global") {
+  //     // Redirect to the homepage if locale is not allowed
+  //     router.replace("/");
+  //   }
+  // }, [router.locale]);
   return (
     <div className="flex flex-col gap-0 md:gap-20">
       <StrategicReviewSection />
-      <BasicModal isOpen={isOpen} onAction={onAction} />
+      {/* <BasicModal isOpen={isOpen} onAction={onAction} /> */}
     </div>
   );
 }
