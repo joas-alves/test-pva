9c2be05914008624588a1bed0c2c49b84f10b6ff
Abdullah <mabdullahkhan009@gmail.com>
2025-04-04 18:36:01 +0500

    fix(words): change pdfs names to pascal case

--- src/components/investors/GeneralMeetingSection.tsx
+++ src/components/investors/GeneralMeetingSection.tsx
@@ -25,16 +25,16 @@ export const GeneralMeetingSection = () => {
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2025</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
           <MeetingLink pdfUrl='/pdfs/2025-AGM-Notice-Final.pdf' name="2025-AGM-Notice-Final" downloadPdf={downloadPdf}/>
-          <MeetingLink pdfUrl='/pdfs/2024-PVG-Annual-Report-FINAL.pdf' name="2024-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
-          <MeetingLink pdfUrl='/pdfs/2025-AGM-Votes-FINAL.pdf' name="2025-AGM-Votes-FINAL" downloadPdf={downloadPdf}/>
-          <MeetingLink pdfUrl='/pdfs/2025-AGM-QA-FINAL.pdf' name="2025-AGM-QA-FINAL" downloadPdf={downloadPdf}/>
+          <MeetingLink pdfUrl='/pdfs/2024-PVG-Annual-Report-FINAL.pdf' name="2024-PVG-Annual-Report-Final" downloadPdf={downloadPdf} />
+          <MeetingLink pdfUrl='/pdfs/2025-AGM-Votes-FINAL.pdf' name="2025-AGM-Votes-Final" downloadPdf={downloadPdf}/>
+          <MeetingLink pdfUrl='/pdfs/2025-AGM-QA-FINAL.pdf' name="2025-AGM-QA-Final" downloadPdf={downloadPdf}/>
         </div>
       </div>
       <div className="flex justify-between flex-col md:flex-row border-b border-b-gray-200 py-10">
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2024</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
           <MeetingLink pdfUrl='/pdfs/2024-AGM-Notice-FINAL.pdf' name="2024-AGM-Notice-Final" downloadPdf={downloadPdf}/>
-          <MeetingLink pdfUrl='/pdfs/2023-PVG-Annual-Report-FINAL.pdf' name="2023-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink pdfUrl='/pdfs/2023-PVG-Annual-Report-FINAL.pdf' name="2023-PVG-Annual-Report-Final" downloadPdf={downloadPdf} />
           <MeetingLink pdfUrl='/pdfs/2024-AGM-Votes-FINAL.pdf' name="2024-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
@@ -43,7 +43,7 @@ export const GeneralMeetingSection = () => {
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2023</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
           <MeetingLink pdfUrl='/pdfs/2023-AGM-Notice-FINAL.pdf' name="2023-AGM-Notice-Final" downloadPdf={downloadPdf} />
-          <MeetingLink pdfUrl='/pdfs/2022-PVG-Annual-Report-FINAL-1.pdf' name="2022-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink pdfUrl='/pdfs/2022-PVG-Annual-Report-FINAL-1.pdf' name="2022-PVG-Annual-Report-Final" downloadPdf={downloadPdf} />
           <MeetingLink pdfUrl='/pdfs/2023-pvg-agm-Votes-FINAL-1.pdf' name="2023-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
@@ -52,7 +52,7 @@ export const GeneralMeetingSection = () => {
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2022</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
           <MeetingLink pdfUrl='/pdfs/2022-PVG-AGM-Notice-of-Meeting-FINAL-2.pdf' name="2022-AGM-Notice-Final" downloadPdf={downloadPdf} />
-          <MeetingLink pdfUrl='/pdfs/2021-PVG-Annual-Report-Web-2.pdf' name="2021-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink pdfUrl='/pdfs/2021-PVG-Annual-Report-Web-2.pdf' name="2021-PVG-Annual-Report-Final" downloadPdf={downloadPdf} />
           <MeetingLink pdfUrl='/pdfs/2022-PVG-AGM-Votes-FINAL.pdf' name="2022-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
@@ -61,7 +61,7 @@ export const GeneralMeetingSection = () => {
         <div className="text-[32px] font-bold text-primary mb-2">AGM 2021</div>
         <div className="max-w-[847px] w-full flex flex-col gap-4">
           <MeetingLink pdfUrl='/pdfs/2021-AGM-Notice-FINAL-web.pdf' name="2021-AGM-Notice-Final" downloadPdf={downloadPdf} />
-          <MeetingLink pdfUrl='/pdfs/2020-PVG-ARA-30-Sept-FINAL-web.pdf' name="2020-PVG-Annual-Report-FINAL" downloadPdf={downloadPdf} />
+          <MeetingLink pdfUrl='/pdfs/2020-PVG-ARA-30-Sept-FINAL-web.pdf' name="2020-PVG-Annual-Report-Final" downloadPdf={downloadPdf} />
           <MeetingLink pdfUrl='/pdfs/2021-AGM-Proxy-Results-for-Web-Site-1.pdf' name="2021-AGM-Votes-Final" downloadPdf={downloadPdf} />
         </div>
       </div>
