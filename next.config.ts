35d279e57f394f6f96517c53bdacfa9d55b0e3da
andriivoron <andriivoron057@gmail.com>
2025-04-20 23:06:36 -0300

    add vercel url to config

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "pva-amber.vercel.app",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   async redirects() {
